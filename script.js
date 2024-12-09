df4246781fe7feaf8ffe2bf1dd14b861253a45b9
victorguimaraesdev <victorguimaraemax@gmail.com>
2024-12-09 19:05:58 -0300

    Adicionado mais enventos

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-
-// .flip - card.flipado.flip - card - inner {
-//     transform: rotateY(180deg);
-// }
-// <script>
-//     const button = document.getElementById ("button")
-//     button.onclick = function(){
-//     	const flip = document.getElementById ("flip")
-//     flip.classList.toggle("flipado")
-//     }
-// </script>
-// </body >
-// </html >
-const efeitoTrocaDeCor = document.getElementById("logar")
-const efeitoTrocaDeCorEscrevase = document.getElementById("escrevase")
-const efeitoTrocaDeCorLogo = document.getElementById("imagem")
+const efeitoTrocaDeCor = document.getElementById("logar");
+const efeitoTrocaDeCorEscrevase = document.getElementById("escrevase");
+const efeitoTrocaDeCorLogo = document.getElementById("imagem");
+const efeitoTrocaDeCorBox = document.getElementsByClassName("box2");
 
 efeitoTrocaDeCor.addEventListener("mouseover", () => {
-    efeitoTrocaDeCor.style.color = "#ffffff"
-    efeitoTrocaDeCor.style.border = "2px solid #ffffff "
-})
+    efeitoTrocaDeCor.style.color = "#ffffff";
+    efeitoTrocaDeCor.style.border = "2px solid #ffffff";
+});
 
 efeitoTrocaDeCor.addEventListener("mouseout", () => {
-    efeitoTrocaDeCor.style.color = "#c7c7c7"
-    efeitoTrocaDeCor.style.border = "2px solid #5c5b5b "
-})
-efeitoTrocaDeCorEscrevase.addEventListener("mouseover", () => {
-    efeitoTrocaDeCorEscrevase.style.color = "#ffffff"
-})
-efeitoTrocaDeCorEscrevase.addEventListener("mouseout", () => {
-    efeitoTrocaDeCorEscrevase.style.color = "#c7c7c7"
-})
-efeitoTrocaDeCorLogo.addEventListener("mouseover", () => {
-    efeitoTrocaDeCorLogo.style.filter = "brightness(.5)"
-    efeitoTrocaDeCorLogo.style.cursor = "pointer"
-})
-efeitoTrocaDeCorLogo.addEventListener("mouseout", () => {
-    efeitoTrocaDeCorLogo.style.filter = "none"
-})
+    efeitoTrocaDeCor.style.color = "#c7c7c7";
+    efeitoTrocaDeCor.style.border = "2px solid #5c5b5b";
+});
 
+efeitoTrocaDeCorEscrevase.addEventListener("mouseover", () => {
+    efeitoTrocaDeCorEscrevase.style.color = "#ffffff";
+});
+
+efeitoTrocaDeCorEscrevase.addEventListener("mouseout", () => {
+    efeitoTrocaDeCorEscrevase.style.color = "#c7c7c7";
+});
+
+efeitoTrocaDeCorLogo.addEventListener("mouseover", () => {
+    efeitoTrocaDeCorLogo.style.filter = "brightness(0.5)";
+    efeitoTrocaDeCorLogo.style.cursor = "pointer";
+});
+
+efeitoTrocaDeCorLogo.addEventListener("mouseout", () => {
+    efeitoTrocaDeCorLogo.style.filter = "none";
+});
+
+
+
+for (let i = 0; i < efeitoTrocaDeCorBox.length; i++) {
+
+    const box = efeitoTrocaDeCorBox[i];
+
+    box.addEventListener("mouseover", () => {
+        box.style.border = "2px solid #ffffff";
+    });
+
+    box.addEventListener("mouseout", () => {
+        box.style.border = "2px solid #5c5b5b";
+    });
+}
